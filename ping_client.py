3f23b0311fa6007134f30627e26d860aa2b6b925
John Munene <jnmunenendwiga@gmail.com>
2022-06-02 01:35:06 +0300

    Update ping_client.py

--- ping_client.py
+++ ping_client.py
@@ -5,14 +5,14 @@ def sendMessage():
   s=socket.socket() #create socket
 
   # server port to send messages to
-  port = 7000
+  port=7000
 
   # connect to server on a local machine
   s.connect(('127.0.0.1',port))
-  print("Client successfully connected to server:")
+  print("Client successfully connected to server")
   
   # prompt user to send message
-  message =input("Enter your message")
+  message=input("Enter your message")
   s.send(message.encode())
 
   # receive response from server
