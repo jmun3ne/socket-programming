a8b65d3a3410ff9ef7b334ea278fd2f084b12c6a
Munene <jmunene.dev@gmail.com>
2022-06-29 23:31:23 +0300

    First commit

---
+++ src/main/java/com/munene/crawler/Crawler.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.munene.crawler;
+
+/**
+ *
+ * @author munene
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*; 
+
+public class Crawler{
+	public static void main (String[] args) throws IOException {
+	Socket s = new Socket("localhost",4999);
+
+	PrintWriter pr = new PrintWriter(s.getOutputStream());
+
+	Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+	System.out.print("Enter message: ");  
+	String stri= sc.nextLine();      
+	pr.println(stri);
+	pr.flush();
+
+	InputStreamReader in = new InputStreamReader(s.getInputStream());
+	BufferedReader bf = new BufferedReader(in);
+
+	String str = bf.readLine();
+	System.out.println("Server: "+ str);
+
+}
+
+}
---
+++ src/main/java/com/munene/crawler/Server.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.munene.crawler;
+
+/**
+ *
+ * @author munene
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*; 
+public class Server {
+    	public static void main (String[] args) throws IOException {
+	ServerSocket ss = new ServerSocket(4999);
+	Socket s = ss.accept();
+
+	System.out.println("Client connected ");
+
+	InputStreamReader in = new InputStreamReader(s.getInputStream());
+	BufferedReader bf = new BufferedReader(in);
+
+	String str = bf.readLine();
+	System.out.println("Client: "+ str);
+
+	PrintWriter pr = new PrintWriter(s.getOutputStream());
+		
+		pr.println(str.toUpperCase());
+		pr.flush();
+
+	}
+}
